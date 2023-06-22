ffd3d2c875e1e08036360a49e435b615a1155604
Md Tariful Islam <108473813+tariful55@users.noreply.github.com>
2023-06-23 02:03:11 +0600

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -1,3 +1,4 @@
+#1
 print("print 11 to 100 using while loop:")
 
 print("11 to 100 are printed below:")
@@ -6,11 +7,14 @@ while i<=100:
 	print(i,i+1,i+2,i+3,i+4)
 	i=i+5
 
+#2
 print("Calculate the sum from 1 to a given number:")
 print("Input the last number")
 n=int(input())
 print("Sum from 1 to",n,"=",n*(n+1)/2)
 
+
+#3
 print("Find the fictorial of a given number")
 print("Input the  number")
 n=int(input())
@@ -22,6 +26,8 @@ while n:
 print("Fictorial of",k, "=", x)
 
 
+
+#4
 print("printing all even number in a range:")
 print("Input the intial number")
 a=int(input())
@@ -33,13 +39,15 @@ while a<=b:
 	print(a)
 	a=a+2
 
+
+#5
 print("Printing the multiplication table of a given number:")
 print("Input the  number")
 x=int(input())
 for k in range(1,11):
 	print(x,"×",k,"=",x*k)
 
-
+#6
 print("A simple calculator using function:")
 print("Input 2 number")
 p,q=map(int,input().split())
